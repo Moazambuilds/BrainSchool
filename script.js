537f641adca36a2a030beaf79262b157765ac62d
Moazam Builds <moazamazam389@mail.com>
2026-08-27 22:58:20 +0500

    header + about ,media,allumini pages complete

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
- document.addEventListener("DOMContentLoaded", function () {
+// Reload header and footer first
+document.addEventListener("DOMContentLoaded", function () {
 
-    const links = document.querySelectorAll(".menu ul a:not(.no-active)");
+    fetch("/reload/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
 
-    links.forEach(link => {
+            // Header load hone ke BAAD ye code chalega
+            const links = document.querySelectorAll(".menu ul a:not(.no-active)");
 
-        if (link.href === window.location.href) {
-            link.classList.add("active");
-        }
+            links.forEach(link => {
+                if (link.href === window.location.href) {
+                    link.classList.add("active");
+                }
+            });
 
-    });
+            const searchopen = document.querySelector(".opensearch");
+            const searchbox1 = document.querySelector(".searchbox");
+            const searchclose = document.querySelector(".closesearch");
+
+            searchopen.addEventListener("click", function () {
+                searchbox1.style.display = "flex";
+            });
+
+            searchclose.addEventListener("click", function () {
+                searchbox1.style.display = "none";
+            });
+        });
+
+
+    fetch("/reload/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
 
 });
+
+
+// Header scroll
 window.addEventListener("scroll", function () {
 
     const header = document.querySelector(".header");
